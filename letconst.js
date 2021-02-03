c00785e697049de19d4fdbd5c02110c0966bcff3
Muhashinul Islam <joymuhashin789@gmail.com>
2021-02-04 02:00:05 +0600

    committed

---
+++ letconst.js
@@ -0,0 +1,18 @@
+// const hubby = "Elias Kanchon";
+
+// console.log(hubby)
+
+// const numbers = [10, 12]
+// numbers[1] = 88;
+// numbers.push(12);
+// console.log(number)
+
+let patientName = "Rahim Chacha"
+patientName = "Fatema Khala";
+console.log(patientName);
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+    
+}
+console.log(i)
---
+++ template.js
@@ -0,0 +1,5 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + ' ' +lastName + ' is a good boy';
+const fullName2 = `${firstName} ${lastName} is a good Boy`
+console.log(fullName2)
