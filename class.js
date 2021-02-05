7aea6b0ff41cb2f95ea3dd657c8e76f4ff5a6507
Muhashinul Islam <joymuhashin789@gmail.com>
2021-02-05 20:24:15 +0600

    committed

---
+++ class.js
@@ -0,0 +1,13 @@
+class Student{
+    constructor(sID, sName){
+        this.id = 1;
+        this.name = sName;
+        this.school = "kolimunnesa school"
+    }
+
+}
+
+const student1 = new Student(12, "Shuvo");
+const student2 = new Student(22, "mahiya");
+const student3 = new Student(29, "Bappi")
+console.log(student3);
---
+++ destructure.js
@@ -0,0 +1,23 @@
+// const person = { name: 'Jack William', age: 17, job: 'facebooker', gfName: 'Ema Watson', address: 'Kochu Khet', phone: '0171133343', friends: ['Tom Hanks', 'Tom Cruise', 'Tom Yarn'] }
+
+// const {phone, gfName, address, salary} = person;
+
+const complexObject =  {
+    name: 'abe',
+    info: {
+        address: 'kola bagan',
+        leader: 'Tiger leader'
+    }
+}
+
+const {leader} = complexObject.info
+
+// const gfName = person.gfName;
+// const phone = person.phone;
+
+
+// console.log(gfName, phone, address, salary);
+// const friends = ['Sakib khan', 'arman khan', 'aamir khan', 'salman khan'];
+// const [chotoFriend, ...restFriend] = friends;
+
+// console.log(olderF)
